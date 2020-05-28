725a5fcd25ead11166e8ecbc55f39055c86309d5
Piotr Kucharski <suspendmode@gmail.com>
2020-05-28 12:17:31 +0200

    index change

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -18,17 +18,38 @@
                     @click="$refs.add_file.click()"
                     class="bg-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded"
                     type="button"
-            >Upload File</button>
+            >Upload File
+            </button>
             <p v-if="hasError" class="my-2 text-danger">
                 {{ firstError }}
             </p>
             <div class="flex flex-col pt-8 text-center">
-                <div v-for="(file, index) in files" class="w-1/2 p-2 mb-4 rounded shadow-lg" style="-ms-word-wrap: break-word;word-wrap: break-word;">
+                <div v-for="(file, index) in files" class="w-1/2 p-2 mb-4 rounded shadow-lg"
+                     style="-ms-word-wrap: break-word;word-wrap: break-word;">
                     <p><a :href="file.url" target="_blank">{{ file.originalName }}</a></p>
                     <br>
                     <p>
-                        <a href="#" @click.prevent="deleteImage(index)" class="text-danger hover:font-bold no-underline mb-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
+                        <a href="#" @click.prevent="prevIndex(index)"
+                           class="text-danger hover:font-bold no-underline mb-2">
+                            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                                 clip-rule="evenodd">
+                                <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z"/>
+                            </svg>
+                            <
+                        </a>
+                        <a href="#" @click.prevent="nextIndex(index)"
+                           class="text-danger hover:font-bold no-underline mb-2">
+                            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                                 clip-rule="evenodd">
+                                <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"/>
+                            </svg>
+                            >
+                        </a>
+                        <a href="#" @click.prevent="deleteImage(index)"
+                           class="text-danger hover:font-bold no-underline mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round" class="feather feather-trash-2">
                                 <polyline points="3 6 5 6 21 6"></polyline>
                                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
                                 <line x1="10" y1="11" x2="10" y2="17"></line>
@@ -44,7 +65,7 @@
 </template>
 
 <script>
-    import { FormField, HandlesValidationErrors } from 'laravel-nova'
+    import {FormField, HandlesValidationErrors} from 'laravel-nova'
 
     export default {
         mixins: [FormField, HandlesValidationErrors],
@@ -75,9 +96,43 @@
                         app.value = JSON.stringify(app.files);
                     })
             },
+            prevIndex(index) {
+                let indexOf = index;
+                indexOf--;
+                if (indexOf < 0) {
+                    indexOf = 0;
+                }
+                if (index === indexOf) {
+                    return;
+                }
+                this.moveArrayItemToNewIndex(this.files, index, indexOf);
+                this.value = JSON.stringify(this.files)
+            },
+            nextIndex(index) {
+                let indexOf = index;
+                indexOf++;
+                if (indexOf > (this.files.length - 1)) {
+                    indexOf = this.files.length - 1;
+                }
+                if (index === indexOf) {
+                    return;
+                }
+                this.moveArrayItemToNewIndex(this.files, index, indexOf);
+                this.value = JSON.stringify(this.files)
 
+            },
+            moveArrayItemToNewIndex(array, oldIndex, newIndex) {
+                if (newIndex >= array.length) {
+                    let k = newIndex - array.length + 1;
+                    while (k--) {
+                        array.push(undefined);
+                    }
+                }
+                array.splice(newIndex, 0, array.splice(oldIndex, 1)[0]);
+                return array;
+            },
             deleteImage(index) {
-                axios.delete('/nova-vendor/array-files/delete/'+this.files[index].name)
+                axios.delete('/nova-vendor/array-files/delete/' + this.files[index].name)
                 this.files.splice(index, 1)
                 this.value = JSON.stringify(this.files)
             },
